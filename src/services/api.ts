6d8ba3f8257c9aad738c4641b3120c44e57ee438
Tahir Ali <tahir.ali@coeus-solutions.de>
2024-12-12 19:55:40 +0500

    skip warning

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,6 +5,9 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    'ngrok-skip-browser-warning': 'true'
+  }
 });
 
 // Add auth token to requests
